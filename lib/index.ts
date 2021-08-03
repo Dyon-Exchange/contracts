8eb1fe7f69fae0e5ecf3e2e171a7f4b3f24a94d5
Conor <conorbrosnan10@gmail.com>
2021-08-03 12:07:13 +1000

    new version

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,8 @@ import { Signer } from "ethers";
 import { Provider } from "@ethersproject/providers";
 import abis from "./export.json";
 
+export type { Dyon } from "../typechain";
+
 export function newContract(
   address: string,
   signerOrProvider: Signer | Provider
@@ -10,6 +12,6 @@ export function newContract(
   return Dyon__factory.connect(address, signerOrProvider);
 }
 
-export default function Dyon(signerOrProvider: Signer | Provider) {
+export default function DyonContract(signerOrProvider: Signer | Provider) {
   return Dyon__factory.connect(abis.contracts.Dyon.address, signerOrProvider);
 }
